247c1ab2e106d21b9f240227a8ac038e871cc316
Maxyjjjjj <121864770+Maxyjjjjj@users.noreply.github.com>
2025-07-07 19:29:32 +0300

    Update ModTags.java

--- src/main/java/com/fungoussoup/ancienthorizons/registry/ModTags.java
+++ src/main/java/com/fungoussoup/ancienthorizons/registry/ModTags.java
@@ -4,6 +4,7 @@ import com.fungoussoup.ancienthorizons.AncientHorizons;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.EntityTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
